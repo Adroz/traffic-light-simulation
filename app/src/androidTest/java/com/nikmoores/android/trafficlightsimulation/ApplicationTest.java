26e282f80c413bcac122ce1b21591bd75d40712f
Adroz <nicholas.k.moores@gmail.com>
2016-01-14 14:55:05 +1100

    Add LightController class to handle cycling lights

--- app/src/androidTest/java/com/nikmoores/android/trafficlightsimulation/ApplicationTest.java
+++ app/src/androidTest/java/com/nikmoores/android/trafficlightsimulation/ApplicationTest.java
@@ -19,6 +19,7 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
 
     public void testSuite() {
         lightProgressesThroughSequence();
+        testBasicLightControllerFunctions();
     }
 
     /**
@@ -45,5 +46,56 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         light.advanceOneSecond();
         light.advanceOneSecond();
         assertEquals("Light should stay RED until commanded otherwise", State.RED, light.getState());
+    }
+
+    /**
+     * Test the basic controller system. Lights initialize correctly (one green, one red), then
+     * one light cycles while the other stays red. Both lights should then be red for one second
+     * before the process starts again with the other light.
+     */
+    private void testBasicLightControllerFunctions() {
+        // I should probably separate this test into helper functions to make it neater.
+        LightController controller = new LightController();
+
+        // Initialization test
+        controller.initLights();
+        String expected = "light1-green, light2-red";         // Make sure one is green and the other is red
+        assertEquals("Light1 should be green, while light2 is red", expected, controller.getSystemStateAsString());
+
+        for (int i = 0; i < 26; i++) {
+            assertNotSame("Light1 shouldn't be RED at the " + i + " second",
+                    State.RED, controller.getLight1().getState());
+            assertEquals("Light2 should be RED during light1 changing state",
+                    State.RED, controller.getLight2().getState());
+            controller.advanceOneSecond();
+        }
+        // *** End of light1 cycle ***
+
+        assertEquals("Lights should now both be RED (end of light1 cycle)",
+                State.RED, controller.getLight1().getState());
+        assertEquals("Lights should now both be RED (end of light1 cycle)",
+                State.RED, controller.getLight2().getState());
+        controller.advanceOneSecond();
+
+        // *** Start of light2 cycle ***
+        for (int i = 0; i < 26; i++) {
+            assertEquals("Light1 should stay RED (light2 cycle running)",
+                    State.RED, controller.getLight1().getState());
+            assertNotSame("Light2 shouldn't be RED until end of cycle (" + (i + 1) + "s)",
+                    State.RED, controller.getLight2().getState());
+            controller.advanceOneSecond();
+        }
+        // *** End of light2 cycle ***
+
+        assertEquals("Lights should now both be RED (end of light2 cycle)",
+                State.RED, controller.getLight1().getState());
+        assertEquals("Lights should now both be RED (end of light2 cycle)",
+                State.RED, controller.getLight2().getState());
+        controller.advanceOneSecond();
+
+        assertEquals("Light1 should now be GREEN (light1 cycle started)",
+                State.GREEN, controller.getLight1().getState());
+        assertEquals("Light2 should stay RED  (light1 cycle running)",
+                State.RED, controller.getLight2().getState());
     }
 }
--- app/src/main/java/com/nikmoores/android/trafficlightsimulation/LightController.java
+++ app/src/main/java/com/nikmoores/android/trafficlightsimulation/LightController.java
@@ -7,15 +7,65 @@ public class LightController {
     Light light1;
     Light light2;
 
-//    public Light getLight1();
-//    public String getSystemState();
+    public static final int STANDARD_RED_TIME = 1;
+    public boolean light1Running;
+    public int redCounter = 0;
 
     public void initLights() {
         // make one green, make sure the other is red
+        light1 = new Light(Light.STANDARD_GREEN_TIME, Light.STANDARD_YELLOW_TIME);
+        light2 = new Light(Light.STANDARD_GREEN_TIME, Light.STANDARD_YELLOW_TIME);
+        light1.setToGreen();
+        light1Running = true;
     }
 
+    /**
+     * Tick the lights through their signaling states. When one light sequence reaches its end, both
+     * lights remain RED for one second, before the new light sequence starts (as per the
+     * conditions).
+     */
     public void advanceOneSecond() {
-        //
+        light1.advanceOneSecond();
+        light2.advanceOneSecond();
+        // Check which light is currently running through its sequence
+        if(light1Running){
+            if(light1.getState() == State.RED){
+                // wait one more second before switching lights.
+                if (redCounter == STANDARD_RED_TIME) {
+                    redCounter = 0;
+                    light2.setToGreen();
+                    light1Running = false;
+                } else {
+                    redCounter++;
+                }
+            }
+        }else{
+            if(light2.getState() == State.RED){
+                // wait one more second before switching lights.
+                if (redCounter == STANDARD_RED_TIME) {
+                    redCounter = 0;
+                    light1.setToGreen();
+                    light1Running = true;
+                } else {
+                    redCounter++;
+                }
+            }
+        }
+        // wait 1 second.
+
+        // Add some kind of error check here in case both lights are a combination of GREEN and YELLOW??
+    }
+
+    public String getSystemStateAsString() {
+        return "light1-" + light1.toString() + ", light2-" + light2.toString();
+    }
+
+    public Light getLight1(){
+        return light1;
+    }
+
+    public Light getLight2(){
+        return light2;
     }
 
     public void run() {
